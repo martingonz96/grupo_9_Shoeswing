2d66e5cfa614bcb927f41de087b18ad9e617ebb5
Mora096 <martigonzalezachaval@gmail.com>
2023-01-16 19:33:40 -0300

    corregir ruta multer

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/images')
+      cb(null, 'public/img/products')
     },
     filename: function (req, file, cb) {
       
