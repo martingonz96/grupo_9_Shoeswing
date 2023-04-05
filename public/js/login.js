6f58c2d56edfcc9adf11ed47ffa2a111a141f9b0
Juampy-19 <jpcastellano1990@gmail.com>
2023-04-05 20:43:10 -0300

    agregue las validaciones desde el front para el registro ycorregi del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-    let formulario = document.querySelector('.formulario');
+    let formulario = document.querySelector('.formLogin');
     let inputEmail = document.querySelector('#email');
     let inputPassword = document.querySelector('#password');
 
-    inputEmail.focus();
+    //inputEmail.focus();
 
     inputEmail.addEventListener('blur', (e) => {
         if (inputEmail.value == '') {
@@ -18,11 +18,17 @@ window.onload = function() {
     })
 
     inputPassword.addEventListener('blur', (e) => {
-        if (inputPassword.value < 6) {
+        if (inputPassword.value.length < 6 && inputPassword.value.length > 0) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'La contraseña debe tener al menos 6 caracteres'
+              })
+        } else if (inputPassword.value == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Ingrese su contraseña'
               })
         } else {
             inputPassword.classList.add('is-valid');
---
+++ public/js/register.js
@@ -0,0 +1,102 @@
+window.onload  = function() {
+    let formulario = document.querySelector('.registerForm');
+    let inputFirstName = document.querySelector('.first_name');
+    let inputLastName = document.querySelector('.last_name');
+    let inputEmail = document.querySelector('.inputRegisterEmail');
+    let inputPassword = document.querySelector('.inputRegisterPassword');
+    let inputConfirmPassword = document.querySelector('.inputRegisterConfirmPassword');
+
+    inputFirstName.focus();
+
+    inputFirstName.addEventListener('blur', (e) => {
+        if (inputFirstName.value == '') {
+            inputFirstName.classList.add('is-invalid');
+        } else {
+            inputFirstName.classList.remove('is-invalid');
+            inputFirstName.classList.add('is-valid');
+        }
+    })
+
+    inputLastName.addEventListener('blur', (e) => {
+        if (inputLastName.value == '') { 
+            inputLastName.classList.add('is-invalid');
+        } else {
+            inputLastName.classList.remove('is-invalid');
+            inputLastName.classList.add('is-valid');
+        }
+    })
+
+    inputEmail.addEventListener('blur', (e) => {
+        if (inputEmail.value == '') {
+            inputEmail.classList.add('is-invalid');
+        } else {
+            inputEmail.classList.remove('is-invalid');
+            inputEmail.classList.add('is-valid');
+        }
+    })
+
+    inputPassword.addEventListener('blur', (e) => {
+        if (inputPassword.value.length < 6) {
+            inputPassword.classList.add('is-invalid');
+        } else {
+            inputPassword.classList.remove('is-invalid');
+            inputPassword.classList.add('is-valid');
+        }
+    })
+
+    inputConfirmPassword.addEventListener('blur', (e) => {
+        if (inputConfirmPassword.value != inputPassword.value) {
+            inputConfirmPassword.classList.add('is-invalid');
+        } else {
+            inputConfirmPassword.classList.remove('is-invalid');
+            inputConfirmPassword.classList.add('is-valid');
+        }
+    })
+
+    formulario.addEventListener('submit', (e) => {
+        let errors = [];
+
+        if (inputFirstName.value == '') {
+            errors.push('Debe introducir un nombre')
+            inputFirstName.classList.add('is-invalid');
+        } else {
+            inputFirstName.classList.remove('is-invalid');
+        };
+
+        if (inputLastName.value == '') {
+            errors.push('Debe introducir un apellido')
+            inputLastName.classList.add('is-invalid');
+        } else {
+            inputLastName.classList.remove('is-invalid');
+        };
+
+        if (inputEmail.value == '') {
+            errors.push('Debe introducir un email')
+            inputEmail.classList.add('is-invalid');
+        } else {
+            inputEmail.classList.remove('is-invalid');
+        };
+
+        if (inputPassword.value < 6) {
+            errors.push('La contraseña debe tener 6 caracteres o más')
+            inputPassword.classList.add('is-invalid');
+        } else {
+            inputPassword.classList.remove('is-invalid');
+        };
+
+        if (errors.length > 0) {
+            e.preventDefault();
+
+            let ulErrors = document.querySelector('errorsRegister');
+            ulErrors.classList.add('alert-warning');
+            console.log(errors);
+            for (let i = 0; i < errors.length; i++) {
+                ulErrors.innerHTML += '<li>' + errors[i] + '</li>';
+            }
+        } else {
+            Swal.fire(
+                'Usuario creado correctamente'
+            )
+        }
+    })
+}
