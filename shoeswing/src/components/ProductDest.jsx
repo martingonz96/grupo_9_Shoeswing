c8b1483072acafb5a25a892cff3d75cfa04fcee9
Juampy-19 <jpcastellano1990@gmail.com>
2023-04-27 15:16:05 -0300

    trabaje en el css del dashboard de react agregando estilos

---
+++ shoeswing/src/components/ProductDest.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProductDest(props) {
+    return (
+        <>
+            <article>
+                <img src={`http://localhost:3000/img/products/${props.image}`} alt={props.image} />
+                <h3>{props.name}</h3><br></br>
+                <div id="productDescriptionCard">
+                    <p>{props.description}</p><br></br>
+                    <h4>$ {props.price}</h4><br></br>
+                    <h4>{props.discount} % OFF</h4>
+                </div>
+            </article>
+        </>
+    )
+}
+export default ProductDest;
